29444559ba9c7dce08df41f6cb3b017464140dc2
Anthony Serrano <agserran@uci.edu>
2017-04-23 14:11:10 -0700

    Fixed script not showing some hats

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 	sessionStorage.setItem("47LAsnap1.jpg",["47 Brand Los Angeles Dodgers Snapback - Blue","$29.99","- Royal Blue Crown & Visor","- Gray Undervisor","- Embroidered Dodgers Team Logo on the Front Panel","- 85% Acrylic, 15% Wool"]);
 	sessionStorage.setItem("47Navystrap1.jpg",["47 Brand Cleanup Classic Dad Hat - Navy","$20.99"]);
+	sessionStorage.setItem("47SFgiantssnap1.jpg",["47 Brand San Francisco Giants Snapback - Black","$29.99"]);
 	sessionStorage.setItem("HUFHstrap1.jpg",["HUF Classic H 6-Panel Strapback - Navy, Stone","$20.99"]);
 	sessionStorage.setItem("undef5strikesnap1.jpg",["Undefeated 5 Strike Snapback Hat - Camo","$30.99"]);
 	sessionStorage.setItem("RVCAsnap1.jpg",["RVCA Trucker Snapback - Black","$34.99"]);
@@ -8,18 +9,20 @@ window.onload = function(){
 	sessionStorage.setItem("BulldogWhitestrap1.jpg",["Dog Limited English Bulldog Dad Hat - White","$28.99"]);
 	sessionStorage.setItem("DogDuckstrap1.jpg",["Dog and Duck Strapback Hat - Navy","$20.99"]);
 	sessionStorage.setItem("CaliBearblackstrap1.jpg",["Cali Bear Dad Hat - Black","$24.99"]);
-	sessionStorage.setItem("Hundredsbluesnap1.jpg",["The Hundreds Globe Snapback- Royal, Red, White","$34.99"]);
+	sessionStorage.setItem("Hundredsbluesnap1.jpg",["Los Angeles Hundreds Snapback - Blue","$26.99"]);
+	sessionStorage.setItem("HundredsGlobeSnap1.jpg",["The Hundreds Globe Snapback - Black, Gold","$25.99"]);
+	sessionStorage.setItem("HundredsPlayersnap1.jpg",["The Hundreds Player Snapback - Teal, Maroon","$25.99"]);
 	sessionStorage.setItem("LAblacksnap1.jpg",["47 Brand Los Angeles Dodgers Snapback - Black","$29.99"]);
-	sessionStorage.setItem("LARamssnap1.jpg",["New Era 9Fifty Los Angeles Rams Snapback - Black, Royal, Gold","$29.99"]);		
+	sessionStorage.setItem("LARamssnap1.jpg",["New Era 9Fifty Los Angeles Rams Snapback - Black, Royal, Gold","$29.99"]);
 	sessionStorage.setItem("PinkDolphinsnap1.jpg",["Pink Dolphin Gradient Snapback - Black","$30.99"]);
-	sessionStorage.setItem("TravisMathewCauthensnap1.jpg",["Travis Mathew Cauthen Snapback Hat - Navy","$30.99"]);	
-	sessionStorage.setItem("TravisMathewCauthensnap1.jpg",["Travis Mathew Cauthen Snapback Hat - Navy","$30.99"]);	
-	sessionStorage.setItem("MLBstrap1.jpg",["New Era 9Twenty MLB Umpire Dad Hat - Denim","$28.99"]);	
-	sessionStorage.setItem("NBAstrap1.jpg",["New Era 9Twenty NBA Logo Dad Hat - Denim","$28.99"]);	
+	sessionStorage.setItem("TravisMathewCauthensnap1.jpg",["Travis Mathew Cauthen Snapback Hat - Navy","$30.99"]);
+	sessionStorage.setItem("TravisMathewCauthensnap1.jpg",["Travis Mathew Cauthen Snapback Hat - Navy","$30.99"]);
+	sessionStorage.setItem("MLBstrap1.jpg",["New Era 9Twenty MLB Umpire Dad Hat - Denim","$28.99"]);
+	sessionStorage.setItem("NBAstrap1.jpg",["New Era 9Twenty NBA Logo Dad Hat - Denim","$28.99"]);
 	sessionStorage.setItem("NikeCamostrap1.jpg",["Nike Dad Hat - Camo","$35.99"]);
-	sessionStorage.setItem("StussyMWstrap1.jpg",["Stussy Melton Wool Strapback - Cardinal","$34.99"]);	
 	sessionStorage.setItem("StussyMWstrap1.jpg",["Stussy Melton Wool Strapback - Cardinal","$34.99"]);
-	sessionStorage.setItem("TupacBearstrap1.jpg",["By Any Meme Tupac Bear Dad Hat - White","$24.99"]);	
+	sessionStorage.setItem("StussyMWstrap1.jpg",["Stussy Melton Wool Strapback - Cardinal","$34.99"]);
+	sessionStorage.setItem("TupacBearstrap1.jpg",["By Any Meme Tupac Bear Dad Hat - White","$24.99"]);
 };
 function itemclicked(name){
 	sessionStorage.setItem("item", name);
