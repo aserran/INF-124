42cb28a5137db1223024787aa3b1901d321f193f
Francisco <arcaf@uci.edu>
2017-04-25 03:33:35 -0700

    Finished detail page with validation and email 4/25

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
+function addquantity(){
+	var divtotal = document.getElementById('total');
+	divtotal.innerHTML = "$"+sessionStorage.getItem(sessionStorage.getItem("item")+"1.jpg").split(",")[1]*parseInt(document.getElementById('quantity').value);
+	document.getElementById('shipmethod').selectedIndex = 0;
+}
 function itemclicked(name){
 	sessionStorage.setItem("item", name);
 }
-function validateform(){
-	
-}
+
 function select(value){
 	if(value == 'overnight'){
 		var divtotal = document.getElementById('total');
-		divtotal.innerHTML = "$"+(parseFloat(sessionStorage.getItem(sessionStorage.getItem("item")+"1.jpg").split(",")[1])+24.00).toFixed(2);
+		divtotal.innerHTML = "$"+((parseFloat(sessionStorage.getItem(sessionStorage.getItem("item")+"1.jpg").split(",")[1])*parseInt(document.getElementById('quantity').value))+24.00).toFixed(2);
 	}
 	else if(value == 'onetothree'){
 		var divtotal = document.getElementById('total');
-		divtotal.innerHTML = "$"+(parseFloat(sessionStorage.getItem(sessionStorage.getItem("item")+"1.jpg").split(",")[1])+10.00).toFixed(2);
+		divtotal.innerHTML = "$"+((parseFloat(sessionStorage.getItem(sessionStorage.getItem("item")+"1.jpg").split(",")[1])*parseInt(document.getElementById('quantity').value))+10.00).toFixed(2);
 	}
-	else{
+	else if(value == 'standard'){
 		var divtotal = document.getElementById('total');
-		divtotal.innerHTML = "$"+sessionStorage.getItem(sessionStorage.getItem("item")+"1.jpg").split(",")[1];
-
+		divtotal.innerHTML = "$"+sessionStorage.getItem(sessionStorage.getItem("item")+"1.jpg").split(",")[1]*parseInt(document.getElementById('quantity').value);
 	}
 }
